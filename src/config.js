93d6ce9282cfe00c257140a010421aca4e18b37d
ebobola-web <romawot94@gmail.com>
2023-11-27 22:20:26 +0300

    yaoi

--- src/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	database_host: '127.0.0.1',
-	database_user: 'postgres',
-	database_password: 'nr?VEA%D7io%g75G',
-	database_port: 5432,
-	server_port: 4552,
-	database_name: 'work_evaluation_system',
-	files_table: 'files'
-};
